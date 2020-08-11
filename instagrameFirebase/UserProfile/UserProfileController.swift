d4b352c0473ab3cdecf4c59c0ecbcc26ce13619c
Ahmed Mohamed Yousef <a41155098@gmail.com>
2020-08-11 15:57:01 +0200

    User Pagination 2

--- instagrameFirebase/UserProfile/UserProfileController.swift
+++ instagrameFirebase/UserProfile/UserProfileController.swift
@@ -59,18 +59,21 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
         
         guard let uid = user?.uid else { return }
         let ref = Database.database().reference().child("posts").child(uid)
-        var query = ref.queryOrderedByKey()
+//        var query = ref.queryOrderedByKey()
+        var query = ref.queryOrdered(byChild: "creationDate")
         if posts.count > 0 {
-            let value = posts.last?.id
-            query = query.queryStarting(atValue: value)
+            let value = posts.last?.creationDate.timeIntervalSince1970
+            query = query.queryEnding(atValue: value)
         }
-        query.queryLimited(toFirst: 4).observeSingleEvent(of: .value, with: { (snapshot) in
+        query.queryLimited(toLast: 4).observeSingleEvent(of: .value, with: { (snapshot) in
             guard var allObjects = snapshot.children.allObjects as? [DataSnapshot] else { return }
-            print("num all objs:",allObjects.count)
+            
+            allObjects.reverse()
+            
             if allObjects.count < 4 {
                 self.isFinishPaging = true
             }
-            if self.posts.count > 0 {
+            if self.posts.count > 0 && allObjects.count > 0{
                 allObjects.removeFirst()
             }
             guard let user = self.user else { return }
@@ -81,6 +84,7 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
                 self.posts.append(post)
             })
             
+               
             self.collectionView.reloadData()
             
         }) { (error) in
